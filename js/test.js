11185fefb2c1ba88793369689d063c179c2efe11
Ethan Thompson <ethanthompson@Ethans-MacBook-Pro.local>
2019-10-14 16:19:39 -0600

    name sync

--- js/test.js
+++ js/test.js
@@ -1,3 +1,4 @@
+
 /*
 (function(){
     if(localStorage.getItem()){
@@ -15,13 +16,26 @@
 // };
 let mylists = {};
 
-
 function addNewItem(event){
     let name = $("#modalAddNew").val();
-     name = `<div class="listItem">
-                        <div class="listItemTitle" contenteditable="true">${name}</div>
+    if (name == ""){
+        alert("Please enter a name");
+    }
+    else{
+        let id = Math.random();
+        todo(name);
+        todoT(name, id)
+        name = `<div class="listItem" id="${id}">
+                        <div class="listItemTitle" contenteditable="true" onkeyup="todo(this.innerText)">${name}</div>
                    </div>`;
-    $("#listNav").append(name);
+        $("#listContainer").append(name);
+
+        $(`#${id}`).animate({
+            height:'40px',
+            width:'100%'
+        });
+    }
+
     //append to set item instead of overwriting (forEach)
     //localStorage.setItem("listItem", name);
 }
@@ -36,5 +50,32 @@ function newModal(){
     }
 
 }
+//updates content title
+function todo(title){
+    $("#todoTitle").html(title);
+}
+function todoT(title, id){
+    $(`#${id}`).html(title);
+}
+function addNewEvent() {
+    let button = `<button onclick="addNewEvent()"><i class="fas fa-plus-circle"></i>New Event</button>`;
+    let newEvent = `<div class="event">
+                        <p contenteditable="true">change Me</p>
+                        <i class="fas fa-trash-alt"></i>
+                    </div>`;
+    let content = [];
+    content.push(newEvent);
+    $("#todoContent").html(newEvent);
+}
+
+function deleteEvent(el) {
+
+}
+
+
+//Every new list has its content put into an object of lists.
+//Each lists has a unique id to call back to its content
+//function that clears content and updates it with
+//corresponding list
 
 //localStorage.setItem("age", "18");
--- js/themes.js
+++ js/themes.js
@@ -1,112 +1,60 @@
-function redTheme(){
+$("#red").click(function(){
     let darkRed = "#5D080B";
     let red = "#9F171D";
     let pink = "#D72C39";
     let white = "#DED7CE";
     let tan = "#8F6952";
-    $(".navbar").css("background-color", pink);
+    let borderProp = "2px solid #ffffff";
+    $(".navbar").css({"background-color":pink, borderBottom: borderProp});
     $("#listNav").css("background-color", darkRed);
-    $("#addNew").css("background-color", darkRed);
-    $("#addNew").hover(function(){
-        $("#addNew").css({"background-color":red, "border-bottom":`2px solid ${tan}`});
-    });
-    $("#listItem").hover(function(){
-        $("#listItem").css({"background-color":red, "border-bottom":`1px solid ${tan}`});
-    });
+    $("#addNewbutton").css("background-color", darkRed);
     $("#todoContainer").css("background-color", white);
-    // $("").css("background-color", )
-    // $("").css("background-color", )
-}
-function blueTheme() {
+});
+$("#blue").click(function(){
     let darkBlue = "#010626";
     let lightBlue = "#024059";
     let white = "#F2F2F2";
     let grey = "#A6A6A6";
     let black = "#2a2a2a";
+    let borderProp = "2px solid #E0C4F2";
     $(".navbar").css("background-color", lightBlue);
     $("#listNav").css("background-color", darkBlue);
-    $("#addNew").css("background-color", darkBlue);
-    $("#addNew").hover(function(){
-        $("#addNew").css("background-color", grey);
-    });
-    $("#listItem").hover(function(){
-        $("#listItem").css("background-color", grey)
-    });
+    $("#addNewbutton").css("background-color", darkBlue);
     $("#todoContainer").css("background-color", white);
-
-
-}
-function greyTheme(){
-    let black = "#060808";
+});
+$("#grey").click(function(){
     let cyan = "#6AB69D";
-    let lightGrey = "#7B7D7B";
+    let lightGrey = "#D7D7D9";
     let blueGrey = "#334141";
     let darkGrey = "#3F4340";
-    $(".navbar").css("background-color", cyan);
+    let borderProp = "2px solid #E0C4F2";
+    $(".navbar").css({"background-color":cyan, borderBottom: borderProp});
     $("#listNav").css("background-color", blueGrey);
-    $("#addNew").css("background-color", blueGrey);
-    $("#addNew").hover(function(){
-        $("#addNew").css("background-color", darkGrey);
-    });
-    $("#listItem").hover(function(){
-        $("#listItem").css("background-color", darkGrey)
-    });
+    $("#addNewbutton").css("background-color", blueGrey);
     $("#todoContainer").css("background-color", lightGrey);
     // $("").css("background-color", )
-}
-function greenTheme(){
+});
+$("#green").click(function(){
     let grey = "#3E3740";
     let white = "#D7D7D9";
     let darkGreen = "#022601";
     let green = "#6DD93B";
-    let lightGreen = "#9df55d";
-    $(".navbar").css("background-color", lightGreen);
+    let lightGreen = "#69A53E";
+    let borderProp = "2px solid #E0C4F2";
+    $(".navbar").css({"background-color": lightGreen, borderBottom: borderProp});
     $("#listNav").css("background-color", darkGreen);
-    $("#addNew").css("background-color", darkGreen);
-    $("#addNew").hover(function(){
-        $("#addNew").css("background-color", green);
-    });
-    $("#listItem").hover(function(){
-        $("#listItem").css("background-color", green);
-    });
+    $("#addNewbutton").css("background-color", darkGreen);
     $("#todoContainer").css("background-color", white);
-}
-function purpleTheme(){
+});
+$("#purple").click(function(){
     let black = "#190F26";
     let darkPurple = "#392259";
     let purple = "#5A378C";
     let lightPurple = "#8850BF";
+    let borderProp = "2px solid #E0C4F2";
     let white = "#E0C4F2";
-    $(".navbar").css({"background-color":purple, "borderBottom":white});
+    $(".navbar").css({"background-color":purple, "borderBottom":borderProp});
     $("#listNav").css("background-color", darkPurple);
-    $("#addNew").css("background-color", darkPurple);
-    $("#addNew").hover(function(){
-        $("#addNew").css("background-color", lightPurple);
-    });
-    $("#listItem").hover(function(){
-        $("#listItem").css("background-color", lightPurple)
-    });
+    $("#addNewbutton").css("background-color", darkPurple);
     $("#todoContainer").css("background-color", white);
-}
-
-function theme(color){
-    switch (color){
-        case (color === 'red'):
-            redTheme();
-            break;
-        case (color === 'blue'):
-            blueTheme();
-            break;
-        case (color === 'grey'):
-            greyTheme();
-            break;
-        case (color === 'green'):
-            greenTheme();
-            break;
-        case (color === 'purple'):
-            purpleTheme();
-            break;
-        default:
-            break;
-    }
-}
+});
